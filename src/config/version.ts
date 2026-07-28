c36b3785c78cc58c922339b6d96d30c04734d104
John Van Horn <johnvanhorn@Johns-MacBook-Air.local>
2026-07-27 20:39:34 -0500

    Update IVM platform

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,10 +1,6 @@
 export const APP_NAME = "IVM Program";
-
 export const APP_VERSION = "0.9.3";
-
 export const BUILD_NUMBER = "2026.07.27.001";
-
 export const BUILD_DATE = "2026-07-27";
 
 export const VERSION_LABEL = `v${APP_VERSION}`;
-export const BUILD_LABEL = `Build ${BUILD_NUMBER}`;
